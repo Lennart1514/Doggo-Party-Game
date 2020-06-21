2f419a5af82a4b305488dc4a33f41c1fca06620c
Lennart1514 <66320991+Lennart1514@users.noreply.github.com>
2020-06-21 13:42:36 +0200

    Aded Controller selection

--- Title Screen.gd
+++ Title Screen.gd
@@ -5,7 +5,7 @@ extends Control
 # var a = 2
 # var b = "text"
 
-
+var Inputwaiting = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -14,7 +14,34 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
+func _input(event):
+	if Inputwaiting == "Dog" and event != InputEventJoypadButton and event.is_pressed():
+		Global.ControllerDog = event.device
+		$"Start Game/Panel/CenterContainer2/HBoxContainer/Doggo Box/ControllerButtonDog".disabled = false
+		$"Start Game/Panel/CenterContainer2/HBoxContainer/Doggo Box/Label".text = "Controller:"+str(event.device+1)
+		Inputwaiting = null
+		updatePlayButton()
+	if Inputwaiting == "Cat" and event != InputEventJoypadButton and event.is_pressed():
+		Global.ControllerCat = event.device
+		$"Start Game/Panel/CenterContainer2/HBoxContainer/Catto Box/ControllerButtonCat".disabled = false
+		$"Start Game/Panel/CenterContainer2/HBoxContainer/Catto Box/Label".text = "Controller:"+str(event.device+1)
+		Inputwaiting = null
+		updatePlayButton()
 
 func _on_Play_pressed():
 	$"Start Game".popup()
+
+func updatePlayButton():
+	if Global.ControllerCat != null and Global.ControllerDog != null and Global.ControllerCat != Global.ControllerDog:
+		$"Start Game/Panel/CenterContainer2/HBoxContainer/VBoxContainer/Start".disabled = false
+	else:
+		$"Start Game/Panel/CenterContainer2/HBoxContainer/VBoxContainer/Start".disabled = true
+func _on_ControllerButtonCat_pressed():
+	$"Start Game/Panel/CenterContainer2/HBoxContainer/Catto Box/ControllerButtonCat".disabled = true
+	$"Start Game/Panel/CenterContainer2/HBoxContainer/Catto Box/Label".text = "Push a button"
+	Inputwaiting = "Cat"
+
+func _on_ControllerButtonDog_pressed():
+	$"Start Game/Panel/CenterContainer2/HBoxContainer/Doggo Box/ControllerButtonDog".disabled = true
+	$"Start Game/Panel/CenterContainer2/HBoxContainer/Doggo Box/Label".text = "Push a button"
+	Inputwaiting = "Dog"
